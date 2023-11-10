fb1d8772be6222461f231c7b44f8433e30d0560e
biswajit0001 <136676890+biswajit0001@users.noreply.github.com>
2023-11-10 09:18:41 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,38 @@
+function jjjs() {
+    let Op = document.getElementById("obtionber");
+    Op.style.color = "red";
+    
+        let option = document.getElementById("hac");
+    option.style.display= "fixed ";
+    option.style.margin="0"
+    option.style.left="0";
+    option.style.display="block";
+    option.style.bottom="0";
+    option.style.top="0"
+    option.style.backgroundColor ="#545454e4";
+     
+    
+}
+
+
+function opopshow() {
+    let obs = document.getElementById("obtionber");
+    obs.style.display="none";
+    let ob1s = document.getElementById("obtionber1");
+    ob1s.style.display="block";
+    let opops = document.getElementById("oppo")
+    opops.style.display="block"
+    let opops1 = document.getElementById("oppo1")
+    opops1.style.display="none"
+}
+function opophide() {
+    let obh = document.getElementById("obtionber");
+    obh.style.display="block";
+    let ob1h = document.getElementById("obtionber1");
+    ob1h.style.display="none";
+    let opoph = document.getElementById("oppo")
+    opoph.style.display="none"
+    let opoph1 = document.getElementById("oppo1")
+    opoph1.style.display="block"
+    
+}
